0779b931027bb3567d31b84558d823272ece2297
Karthiksuriya01 <karthiksuriy01@gmail.com>
2025-01-22 13:21:17 +0530

    updated

--- requrie-createServer-methods/httpserver.js
+++ requrie-createServer-methods/httpserver.js
@@ -1,7 +1,7 @@
 const http = require("http")
 
 // createServer() is used to   create a web server
-
+//hello wqelcome 
 var server = http.createServer((req,res) => // here res -> response from the user ,req -> request from the user
 {
     res.write("Hello world ")
